c545bd95d13e529b4acc3a583279b786fa4deb85
dmqc7 <duarte.correia@sapo.pt>
2023-03-17 19:58:00 +0000

    fix2

--- app.py
+++ app.py
@@ -98,5 +98,6 @@ def update():
 # End webserver stuff
 ########################################
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000)) # Use port specified by environment variable, or default to 5000
+    app.run(host='0.0.0.0', port=port)
